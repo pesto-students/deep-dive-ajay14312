7505754d201b54838be5c187711518477766718d
ajaykumar-mittakola <ajaykumar.mittakola@bankopen.co>
2020-05-02 00:21:54 +0530

    Capture focus and adjust using keyboard

--- react-exercises/modal-react/src/App.js
+++ react-exercises/modal-react/src/App.js
@@ -4,14 +4,20 @@ import Modal from "./Modal";
 
 function App() {
   const [isVisible, setIsVisible] = useState(false);
-  const modalProps = { align: "center" };
+  const [secondVisible, setSecondVisible] = useState(false);
+  const modalProps = { align: "center", width: '400px' };
   const showModal = () => {
     setIsVisible(true);
-    document.querySelector("body").style.position = "fixed";
   };
   const closeModal = () => {
     setIsVisible(false);
   };
+  const showCheck = () => {
+    setSecondVisible(true);
+  }
+  const check = () => {
+    setSecondVisible(false);
+  }
   return (
     <div className="App">
       <div>
@@ -54,26 +60,112 @@ function App() {
           following is an example of using react-modal specifying all the
           possible props and options:
         </div>
+        <div>
+          react-modal Accessible modal dialog component for React.JS We maintain
+          that accessibility is a key component of any modern web application.
+          As such, we have created this modal in such a way that it fulfills the
+          accessibility requirements of the modern web. We seek to keep the
+          focus on accessibility while providing a functional, capable modal
+          component for general use. Installation To install the stable version
+          you can use npm or yarn: $ npm install react-modal $ yarn add
+          react-modal General Usage The only required prop for the modal object
+          is isOpen, which indicates whether the modal should be displayed. The
+          following is an example of using react-modal specifying all the
+          possible props and options:
+        </div>
+        <div>
+          react-modal Accessible modal dialog component for React.JS We maintain
+          that accessibility is a key component of any modern web application.
+          As such, we have created this modal in such a way that it fulfills the
+          accessibility requirements of the modern web. We seek to keep the
+          focus on accessibility while providing a functional, capable modal
+          component for general use. Installation To install the stable version
+          you can use npm or yarn: $ npm install react-modal $ yarn add
+          react-modal General Usage The only required prop for the modal object
+          is isOpen, which indicates whether the modal should be displayed. The
+          following is an example of using react-modal specifying all the
+          possible props and options:
+        </div>
+        <div>
+          react-modal Accessible modal dialog component for React.JS We maintain
+          that accessibility is a key component of any modern web application.
+          As such, we have created this modal in such a way that it fulfills the
+          accessibility requirements of the modern web. We seek to keep the
+          focus on accessibility while providing a functional, capable modal
+          component for general use. Installation To install the stable version
+          you can use npm or yarn: $ npm install react-modal $ yarn add
+          react-modal General Usage The only required prop for the modal object
+          is isOpen, which indicates whether the modal should be displayed. The
+          following is an example of using react-modal specifying all the
+          possible props and options:
+        </div>
+        <div>
+          react-modal Accessible modal dialog component for React.JS We maintain
+          that accessibility is a key component of any modern web application.
+          As such, we have created this modal in such a way that it fulfills the
+          accessibility requirements of the modern web. We seek to keep the
+          focus on accessibility while providing a functional, capable modal
+          component for general use. Installation To install the stable version
+          you can use npm or yarn: $ npm install react-modal $ yarn add
+          react-modal General Usage The only required prop for the modal object
+          is isOpen, which indicates whether the modal should be displayed. The
+          following is an example of using react-modal specifying all the
+          possible props and options:
+        </div>
+        <div>
+          react-modal Accessible modal dialog component for React.JS We maintain
+          that accessibility is a key component of any modern web application.
+          As such, we have created this modal in such a way that it fulfills the
+          accessibility requirements of the modern web. We seek to keep the
+          focus on accessibility while providing a functional, capable modal
+          component for general use. Installation To install the stable version
+          you can use npm or yarn: $ npm install react-modal $ yarn add
+          react-modal General Usage The only required prop for the modal object
+          is isOpen, which indicates whether the modal should be displayed. The
+          following is an example of using react-modal specifying all the
+          possible props and options:
+        </div>
+        <div>
+          react-modal Accessible modal dialog component for React.JS We maintain
+          that accessibility is a key component of any modern web application.
+          As such, we have created this modal in such a way that it fulfills the
+          accessibility requirements of the modern web. We seek to keep the
+          focus on accessibility while providing a functional, capable modal
+          component for general use. Installation To install the stable version
+          you can use npm or yarn: $ npm install react-modal $ yarn add
+          react-modal General Usage The only required prop for the modal object
+          is isOpen, which indicates whether the modal should be displayed. The
+          following is an example of using react-modal specifying all the
+          possible props and options:
+        </div>
         <button type="button" onClick={showModal}>
           open modal
         </button>
+        <button onClick={showCheck}>check if works</button>
       </div>
 
-      <Modal
+      {isVisible && <Modal
         isVisible={isVisible}
         modalProps={modalProps}
-        panelClass= {['class1', 'class2']}
+        panelClass={["class1", "class2"]}
         onModalClose={closeModal}
       >
         <div>
           <h1>Start</h1>
           <h2>close</h2>
         </div>
+        <select>
+          <option>uo</option>
+          <option>yes</option>
+        </select>
+        <textarea/>
+        
         <input />
         <input />
         <input />
         <input />
-      </Modal>
+      </Modal>}
+      {secondVisible && <Modal isVisible={secondVisible} onModalClose={check}>check</Modal>}
     </div>
   );
 }
--- react-exercises/modal-react/src/Modal.js
+++ react-exercises/modal-react/src/Modal.js
@@ -1,107 +1,146 @@
 import React, { Component } from "react";
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
 import "./Modal.css";
 
-const overlay = document.createElement('div');
-overlay.id = 'overlay-container'
+const overlay = document.createElement("div");
+overlay.id = "overlay-container";
+
+const tabbleList = [
+  "a[href]",
+  "area[href]",
+  "input:not([disabled])",
+  "button:not([disabled])",
+  "[contendeditable]",
+  "textarea:not([disabled])",
+  "select:not([disabled])",
+  "iframe",
+  "object",
+  "embed"
+];
+
 class Modal extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      list: ""
-    };
-    this.element = document.createElement('div');
 
-    this.node = '';
+    this.state = {
+      list: [],
+    };
+
+    this.element = document.createElement("div");
+    this.node = "";
+    this.activeElement = '';
+    if (this.props.isVisible) {
+      document.querySelector("html").style.overflow = 'hidden';
+    }
   }
 
   componentWillUnmount = () => {
-    document.removeEventListener('mousedown', this.handleClick, false);
-    document.removeEventListener('keydown', this.captureKey, false);
+    document.removeEventListener("mousedown", this.handleClick, false);
+    document.removeEventListener("keydown", this.captureKey, false);
     overlay.removeChild(this.element);
-  }
+    document.querySelector("html").style.overflow = 'auto';
+  };
 
   handleClick = (e) => {
-    if(this.node.contains(e.target)) {
+    if (this.node.contains(e.target)) {
       return;
     }
-    if(document.activeElement.toString() === "[object HTMLBodyElement]" && e.keyCode === 27) {
-      this.props.onModalClose();
-    }
-
-    this.props.onModalClose();
-  }
+    // if ( document.activeElement.toString() === "[object HTMLBodyElement]" && e.keyCode === 27) {
+    //   this.props.onModalClose();
+    //   this.activeElement.focus();
+    // }
+    this.closeOverlay();
+  };
 
   componentDidMount = () => {
-    document.addEventListener('mousedown', this.handleClick, false);
-    document.addEventListener('keydown', this.captureKey, false);
-    document.querySelector('body').appendChild(overlay);
+    this.activeElement = document.activeElement;
+    document.addEventListener("mousedown", this.handleClick, false);
+    document.addEventListener("keydown", this.captureKey, false);
+    document.querySelector("body").appendChild(overlay);
     overlay.appendChild(this.element);
+
     this.setState(() => {
+      const arrList = Array.prototype.slice.call(document.getElementById("childrenDiv").querySelectorAll(tabbleList))
       return {
-        list: document
-          .getElementById("childrenDiv")
-          .querySelectorAll(["input", "button"]),
+        list: arrList
       };
     });
-    setTimeout(() => {
-      document
-          .getElementById("childrenDiv")
-          .querySelectorAll(["input", "button"])[0].focus();
-    }, 500)
   };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevProps.isVisible === this.props.isVisible &&
+      this.props.isVisible === true) {
+        this.focusFirstElement();
+    }
+  };
+
+  focusFirstElement = () => {
+    if (this.state.list[1]) {
+      this.state.list[1].focus();
+    } else {
+      this.state.list[0].focus();
+    }
+  }
 
   captureKey = (event) => {
-    //event.persist();
     if (event.keyCode === 9) {
-      if (
-        document.activeElement === this.state.list[this.state.list.length - 1]
-      ) {
-        this.state.list[0].focus();
+      if (!this.state.list.includes(document.activeElement)) {
+        event.preventDefault();
+        this.focusFirstElement();
+      } else if (event.shiftKey) {
+        if (document.activeElement === this.state.list[0]) {
+          event.preventDefault();
+          this.state.list[this.state.list.length - 1].focus();
+        }
+      } else {
+        if (document.activeElement === this.state.list[this.state.list.length - 1]) {
+          event.preventDefault();
+          this.state.list[0].focus();
+        }
       }
-    }
-    if (event.shiftKey && event.key === "Tab") {
-      console.log(event.keyCode);
-      if (document.activeElement === this.state.list[0]) {
-        this.state.list[this.state.list.length - 1].focus();
-      }
-    }
-
-    if (event.keyCode === 27) {
-      this.props.onModalClose();
+    } else if (event.keyCode === 27) {
+      this.closeOverlay();
     }
   };
 
+  closeOverlay = () => {
+    this.props.onModalClose();
+    this.activeElement.focus();
+  }
+
   render() {
-    let showHideClassName =this.props.isVisible
-        ? "modal display-block"
-        : "modal display-none";
+    const showHideClassName = this.props.isVisible ? "show" : "display-none";
+    const alignModal = this.props.modalProps && this.props.modalProps.align? this.props.modalProps.align : 'center';
+
     if (!showHideClassName) {
       return null;
-    };
-
-    let customClassNames = '';
-    try {
-      customClassNames = this.props.panelClass.join(',');
-    } catch (_) {
-      // do nothing
     }
 
-    let overlayNode = <React.Fragment>
+    const customClassNames = this.props.panelClass?this.props.panelClass.join(' '):'';
+
+    const overlayNode = (
       <div
-        className={showHideClassName}
+        className={`modal ${showHideClassName}`}
         onKeyDown={this.captureKey}
         id="childrenDiv"
-      >
-        <section className={`modal-main ${customClassNames}`} ref={node => this.node = node}>
-        {this.props.children}
-        </section>
+        role="dialog">
+        <div className={`modal-content ${alignModal === 'left'?'modal-content-left':''} ${alignModal === 'right'?'modal-content-right':'' } ${customClassNames} ${alignModal === 'center'?'modal-content-center':''}`} ref={(node) => (this.node = node)}>
+          <div className="modal-header">
+            <button className="close" onClick={this.closeOverlay}>&times;</button>
+            <h2>Modal Header</h2>
+          </div>
+          <div className="modal-body">
+            {this.props.children}
+          </div>
+          <div className="modal-footer">
+            
+            <h3>Modal Footer</h3>
+          </div>
+        </div>
       </div>
-    </React.Fragment>
-    
-    return (
-      ReactDOM.createPortal(overlayNode, this.element)
     );
+
+    return ReactDOM.createPortal(overlayNode, this.element);
   }
 }
 
