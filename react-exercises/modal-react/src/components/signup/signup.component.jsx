9bd24caf861329ff4acbf0af4c57417a65b54cce
ajaykumar-mittakola <ajaykumar.mittakola@bankopen.co>
2020-05-02 16:18:49 +0530

    Add dashboard route and css in signup

--- react-exercises/modal-react/src/components/signup/signup.component.jsx
+++ react-exercises/modal-react/src/components/signup/signup.component.jsx
@@ -1,22 +1,43 @@
-import React from 'react';
-import './signup.style.css';
+import React from "react";
+import "./signup.style.css";
+import { useHistory } from "react-router-dom";
+import Modal from "../../Modal";
 
 export const SignupComponent = (props) => {
+  const history = useHistory();
+  const handleSubmit = () => {
+    history.push("/dashboard");
+  };
+
   return (
-    <div className="signup-form">
-      <h3 className="text-center"> Signup Form</h3>
-      <form action="dashboard">
-        <label htmlFor="email">Email</label>
-        <input type="email" id="fname"  placeholder="Your email.."/>
+    <React.Fragment>
+      <Modal
+        isVisible={props.isSignupVisible}
+        panelClass={["class1", "class2"]}
+        onModalClose={props.onModalClose}
+        modalProps={{ width: "80%", overlayClickClose: true, Enter: false }}
+        onModalConfirmation={handleSubmit}
+      >
+        <div className="signup-form">
+          <h3 className="text-center"> Signup Form</h3>
+          <form>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="fname" placeholder="Your email.." />
 
-        <label htmlFor="pwd">Password</label>
-        <input type="password" id="pwd"  placeholder="Your password.."/>
+            <label htmlFor="pwd">Password</label>
+            <input type="password" id="pwd" placeholder="Your password.." />
 
-        <label htmlFor="confirm-pwd">Confirm Password</label>
-        <input type="password" id="confirm-pwd"  placeholder="Confirm password.."/>
+            <label htmlFor="confirm-pwd">Confirm Password</label>
+            <input
+              type="password"
+              id="confirm-pwd"
+              placeholder="Confirm password.."
+            />
 
-        <input type="submit" value="Submit"/>
-      </form>
-    </div>
-  )
-}
+            {/* <input type="submit" value="Submit" onClick={handleSubmit} /> */}
+          </form>
+        </div>
+      </Modal>
+    </React.Fragment>
+  );
+};
