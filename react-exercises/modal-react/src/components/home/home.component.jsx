d8e8f9715dc2515148ec052c0c1bf8b5d0410841
ajaykumar-mittakola <ajaykumar.mittakola@bankopen.co>
2020-05-02 16:19:31 +0530

    Refactor home page and login page

--- react-exercises/modal-react/src/components/home/home.component.jsx
+++ react-exercises/modal-react/src/components/home/home.component.jsx
@@ -1,38 +1,32 @@
-
-import React from 'react';
+import React from "react";
 import Modal from "../../Modal";
-import { SignupComponent } from '../signup/signup.component';
-import { LoginComponent } from '../login/login.component';
-import { Header } from '../header/header.componet';
-import {DummyText} from '../dumy-text/dumy-text.component';
-import './home.style.css';
+import { SignupComponent } from "../signup/signup.component";
+import { LoginComponent } from "../login/login.component";
+import { Header } from "../header/header.componet";
+import { DummyText } from "../dumy-text/dumy-text.component";
+import "./home.style.css";
 
 export const Home = (props) => {
   return (
     <div className="App">
-      <Header openSignup ={props.openSignup} openLogin={props.openLogin}/>
-      <DummyText/>
-      
-      {props.isLoginVisible && <Modal
-        isVisible={props.isLoginVisible}
-        modalProps={props.modalProps}
-        panelClass={["class1", "class2"]}
-        onModalClose={props.onModalClose}
-      >
-        <LoginComponent handleSubmit ={props.handleSubmit }/>
-      </Modal>}
+      <Header openSignup={props.openSignup} openLogin={props.openLogin} />
+      <DummyText />
 
-      {props.isSignupVisible && <Modal
-        isVisible={props.isSignupVisible}
-        modalProps={props.modalProps}
-        panelClass={["class1", "class2"]}
-        onModalClose={props.onModalClose}
-      >
-        <SignupComponent handleSubmit ={props.handleSubmit }/>
-      </Modal>}
+      {props.isLoginVisible && (
+        <LoginComponent
+          handleSubmit={props.handleSubmit}
+          isLoginVisible={props.isLoginVisible}
+          onModalClose={props.onModalClose}
+        />
+      )}
 
-      {/* {secondVisible && <Modal isVisible={secondVisible} onModalClose={check}>check</Modal>} */}
+      {props.isSignupVisible && (
+        <SignupComponent
+          handleSubmit={props.handleSubmit}
+          isSignupVisible={props.isSignupVisible}
+          onModalClose={props.onModalClose}
+        />
+      )}
     </div>
   );
-}
-
+};
--- react-exercises/modal-react/src/components/login/login.component.jsx
+++ react-exercises/modal-react/src/components/login/login.component.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
-import './login.style.css';
+import React from "react";
+import "./login.style.css";
+import { useHistory } from "react-router-dom";
+import Modal from "../../Modal";
 
 export const LoginComponent = (props) => {
+  const history = useHistory();
+  const handleSubmit = () => {
+    history.push("/dashboard");
+  };
+
+  const modalProps = {
+    align: "left",
+    esc: false,
+    overlayClickClose: false,
+    footerInclude: false,
+  };
+
   return (
-    <div className="login-form">
-      <h3 className="text-center"> Login Form</h3> 
-      <form action="/dashboard">
-        <label htmlFor="email">Email</label>
-        <input type="email" id="fname"  placeholder="Your email.."/>
+    <React.Fragment>
+      <Modal
+        isVisible={props.isLoginVisible}
+        ModalTitle="Login"
+        modalProps={modalProps}
+        panelClass={["class1", "class2"]}
+        onModalClose={props.onModalClose}
+        onModalConfirmation={handleSubmit}
+      >
+        <div className="login-form">
+          <h3 className="text-center"> Login Form</h3>
+          <form>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="fname" placeholder="Your email.." />
 
-        <label htmlFor="pwd">Password</label>
-        <input type="password" id="pwd"  placeholder="Your password.."/>
+            <label htmlFor="pwd">Password</label>
+            <input type="password" id="pwd" placeholder="Your password.." />
 
-        <input type="submit" value="Submit"/>
-      </form>
-    </div>
-  )
-}
+            <input type="submit" value="Submit" onClick={handleSubmit} />
+          </form>
+        </div>
+      </Modal>
+    </React.Fragment>
+  );
+};
