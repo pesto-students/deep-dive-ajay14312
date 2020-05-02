f66b39f3b52c7a10808ef5718a24e57818f11468
ajaykumar-mittakola <ajaykumar.mittakola@bankopen.co>
2020-05-02 17:21:11 +0530

    Adjust header css and rename header css file

--- react-exercises/modal-react/src/components/header/header.componet.jsx
+++ react-exercises/modal-react/src/components/header/header.componet.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import './header.style.css';
+import React from "react";
+import "./header.css";
 
 export const Header = (props) => {
   return (
     <div className="header">
-      <button className="btn home">Modal Demo</button>
-      <button className="btn" onClick={props.openSignup}>Register</button>
-      <button className="btn" onClick={props.openLogin}>Login</button>
-  </div>
-  )
-}
+      <button className="btn home">Modal</button>
+      <button className="btn" onClick={props.openSignup}>
+        Register
+      </button>
+      <button className="btn" onClick={props.openLogin}>
+        Login
+      </button>
+    </div>
+  );
+};
