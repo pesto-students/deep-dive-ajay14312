5dccf11769930ed8312ef2c23207af75e1c90cec
ajaykumar-mittakola <ajaykumar.mittakola@bankopen.co>
2020-05-02 16:20:40 +0530

    Refactor App page and dummy text page

--- react-exercises/modal-react/src/App.js
+++ react-exercises/modal-react/src/App.js
@@ -1,32 +1,17 @@
 import React, { useState } from "react";
 import "./App.css";
-import Modal from "./Modal";
-import { BrowserRouter, Route, re } from 'react-router-dom';
-import { SignupComponent } from './components/signup/signup.component';
-import { LoginComponent } from './components/login/login.component';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Dashboard } from './components/dashboard/dashboard.component';
 import { Home } from './components/home/home.component';
-import { useHistory } from "react-router-dom";
+
 
 function App() {
-  const [isVisible, setIsVisible] = useState(false);
   const [isLoginVisible, setIsLoginVisible] = useState(false);
   const [isSignupVisible, setIsSignupVisible] = useState(false);
-  const [secondVisible, setSecondVisible] = useState(false);
   const modalProps = { align: "center", width: '400px' };
-  const showModal = () => {
-    setIsVisible(true);
-  };
   const closeModal = () => {
-    setIsVisible(false);
     setIsLoginVisible(false);
     setIsSignupVisible(false);
-  };
-  const showCheck = () => {
-    setSecondVisible(true);
-  };
-  const check = () => {
-    setSecondVisible(false);
   };
 
   const openLogin = () => {
@@ -36,19 +21,12 @@ function App() {
   const openSignup = () => {
     setIsSignupVisible(true);
   };
-
-  const handleSubmit = (event) => {
-    // const history = useHistory();
-    // history.push("/dashboard");
-
-    event.preventDefault();
-  };
   return (
     <div className="App">
       <BrowserRouter>
         <Route path='/' exact render={(props) => <Home openSignup={openSignup} openLogin={openLogin}
           modalProps={modalProps} onModalClose={closeModal}
-          isSignupVisible={isSignupVisible} isLoginVisible={isLoginVisible} handleSubmit={handleSubmit}
+          isSignupVisible={isSignupVisible} isLoginVisible={isLoginVisible}
         />} />
         <Route path='/dashboard' component={Dashboard} />
       </BrowserRouter>
--- react-exercises/modal-react/src/components/dumy-text/dumy-text.component.jsx
+++ react-exercises/modal-react/src/components/dumy-text/dumy-text.component.jsx
@@ -1,125 +1,21 @@
-import React from 'react';
+import React from "react";
 
 export const DummyText = (props) => {
   return (
     <div>
       <div>
         react-modal Accessible modal dialog component for React.JS We maintain
-        that accessibility is a key component of any modern web application.
-        As such, we have created this modal in such a way that it fulfills the
-        accessibility requirements of the modern web. We seek to keep the
-        focus on accessibility while providing a functional, capable modal
-        component for general use. Installation To install the stable version
-        you can use npm or yarn: $ npm install react-modal $ yarn add
-        react-modal General Usage The only required prop for the modal object
-        is isOpen, which indicates whether the modal should be displayed. The
-        following is an example of using react-modal specifying all the
-        possible props and options:
+        that accessibility is a key component of any modern web application. As
+        such, we have created this modal in such a way that it fulfills the
+        accessibility requirements of the modern web. We seek to keep the focus
+        on accessibility while providing a functional, capable modal component
+        for general use. Installation To install the stable version you can use
+        npm or yarn: $ npm install react-modal $ yarn add react-modal General
+        Usage The only required prop for the modal object is isOpen, which
+        indicates whether the modal should be displayed. The following is an
+        example of using react-modal specifying all the possible props and
+        options:
       </div>
-      <div>
-        react-modal Accessible modal dialog component for React.JS We maintain
-        that accessibility is a key component of any modern web application.
-        As such, we have created this modal in such a way that it fulfills the
-        accessibility requirements of the modern web. We seek to keep the
-        focus on accessibility while providing a functional, capable modal
-        component for general use. Installation To install the stable version
-        you can use npm or yarn: $ npm install react-modal $ yarn add
-        react-modal General Usage The only required prop for the modal object
-        is isOpen, which indicates whether the modal should be displayed. The
-        following is an example of using react-modal specifying all the
-        possible props and options:
-      </div>
-      <div>
-        react-modal Accessible modal dialog component for React.JS We maintain
-        that accessibility is a key component of any modern web application.
-        As such, we have created this modal in such a way that it fulfills the
-        accessibility requirements of the modern web. We seek to keep the
-        focus on accessibility while providing a functional, capable modal
-        component for general use. Installation To install the stable version
-        you can use npm or yarn: $ npm install react-modal $ yarn add
-        react-modal General Usage The only required prop for the modal object
-        is isOpen, which indicates whether the modal should be displayed. The
-        following is an example of using react-modal specifying all the
-        possible props and options:
-      </div>
-      <div>
-        react-modal Accessible modal dialog component for React.JS We maintain
-        that accessibility is a key component of any modern web application.
-        As such, we have created this modal in such a way that it fulfills the
-        accessibility requirements of the modern web. We seek to keep the
-        focus on accessibility while providing a functional, capable modal
-        component for general use. Installation To install the stable version
-        you can use npm or yarn: $ npm install react-modal $ yarn add
-        react-modal General Usage The only required prop for the modal object
-        is isOpen, which indicates whether the modal should be displayed. The
-        following is an example of using react-modal specifying all the
-        possible props and options:
-      </div>
-      <div>
-        react-modal Accessible modal dialog component for React.JS We maintain
-        that accessibility is a key component of any modern web application.
-        As such, we have created this modal in such a way that it fulfills the
-        accessibility requirements of the modern web. We seek to keep the
-        focus on accessibility while providing a functional, capable modal
-        component for general use. Installation To install the stable version
-        you can use npm or yarn: $ npm install react-modal $ yarn add
-        react-modal General Usage The only required prop for the modal object
-        is isOpen, which indicates whether the modal should be displayed. The
-        following is an example of using react-modal specifying all the
-        possible props and options:
-      </div>
-      <div>
-        react-modal Accessible modal dialog component for React.JS We maintain
-        that accessibility is a key component of any modern web application.
-        As such, we have created this modal in such a way that it fulfills the
-        accessibility requirements of the modern web. We seek to keep the
-        focus on accessibility while providing a functional, capable modal
-        component for general use. Installation To install the stable version
-        you can use npm or yarn: $ npm install react-modal $ yarn add
-        react-modal General Usage The only required prop for the modal object
-        is isOpen, which indicates whether the modal should be displayed. The
-        following is an example of using react-modal specifying all the
-        possible props and options:
-      </div>
-      <div>
-        react-modal Accessible modal dialog component for React.JS We maintain
-        that accessibility is a key component of any modern web application.
-        As such, we have created this modal in such a way that it fulfills the
-        accessibility requirements of the modern web. We seek to keep the
-        focus on accessibility while providing a functional, capable modal
-        component for general use. Installation To install the stable version
-        you can use npm or yarn: $ npm install react-modal $ yarn add
-        react-modal General Usage The only required prop for the modal object
-        is isOpen, which indicates whether the modal should be displayed. The
-        following is an example of using react-modal specifying all the
-        possible props and options:
-      </div>
-      <div>
-        react-modal Accessible modal dialog component for React.JS We maintain
-        that accessibility is a key component of any modern web application.
-        As such, we have created this modal in such a way that it fulfills the
-        accessibility requirements of the modern web. We seek to keep the
-        focus on accessibility while providing a functional, capable modal
-        component for general use. Installation To install the stable version
-        you can use npm or yarn: $ npm install react-modal $ yarn add
-        react-modal General Usage The only required prop for the modal object
-        is isOpen, which indicates whether the modal should be displayed. The
-        following is an example of using react-modal specifying all the
-        possible props and options:
-      </div>
-      <div>
-        react-modal Accessible modal dialog component for React.JS We maintain
-        that accessibility is a key component of any modern web application.
-        As such, we have created this modal in such a way that it fulfills the
-        accessibility requirements of the modern web. We seek to keep the
-        focus on accessibility while providing a functional, capable modal
-        component for general use. Installation To install the stable version
-        you can use npm or yarn: $ npm install react-modal $ yarn add
-        react-modal General Usage The only required prop for the modal object
-        is isOpen, which indicates whether the modal should be displayed. The
-        following is an example of using react-modal specifying all the
-        possible props and options:
-      </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
