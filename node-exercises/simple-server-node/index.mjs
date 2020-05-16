d47b2fd37386f9fb5d69a530668933e599919b95
ajaykumar-mittakola <ajaykumar.mittakola@bankopen.co>
2020-05-16 13:33:39 +0530

    Add index file and import server

---
+++ node-exercises/simple-server-node/index.mjs
@@ -0,0 +1,12 @@
+import Server from "./lib/server.mjs";
+import getData from "./check/get-data.mjs";
+import postData from "./check/post-data.mjs";
+
+const PORT = process.env.PORT || 4000;
+
+const server = new Server();
+server.listen(PORT);
+
+server.get("/check", getData);
+server.get("/check/:id", getData);
+server.post("/post-data", postData);
