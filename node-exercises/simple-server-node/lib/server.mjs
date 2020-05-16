061d0161b03572e46ab879497425cfdf816def1e
ajaykumar-mittakola <ajaykumar.mittakola@bankopen.co>
2020-05-16 13:31:11 +0530

    Update server funcationality using utility methods

--- node-exercises/simple-server-node/lib/server.mjs
+++ node-exercises/simple-server-node/lib/server.mjs
@@ -1,6 +1,10 @@
 import http from "http";
+import url from "url";
+import bodyParser from "./body-parser.mjs";
+import routeCheck from "./route-check.mjs";
 
-export default class Server {
+const routes = [];
+class Server {
   constructor() {
     this.server = http.createServer();
     this.server.on("request", this.parser);
@@ -11,9 +15,68 @@ export default class Server {
     console.log(`listening on port ${port}`);
   }
 
-  parser = (request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write(request.url);
-    response.end();
+  parser = async (request, response) => {
+    const parsedURL = url.parse(request.url, true);
+    let isRouteExist = false;
+    for (let item of routes) {
+      const routeCheckData = routeCheck(parsedURL.pathname, item);
+      if (routeCheckData.isCorrectRoute && request.method === item.method) {
+        isRouteExist = true;
+        request.queryParams = JSON.stringify(parsedURL.query);
+        request.params = routeCheckData.params;
+        if (request.method !== "GET") {
+          bodyParser(request, (result) => {
+            request.body = result;
+            item.cb(request, response);
+          });
+        } else {
+          await item.cb(request, response);
+        }
+      }
+    }
+
+    if (!isRouteExist) {
+      response.writeHead(404, { "Content-Type": "text/html" });
+      response.write(`<h1>URL not found</h1>`);
+      response.end();
+    }
+  };
+
+  get = (route, cb) => {
+    const routeObj = {
+      route: route,
+      method: "GET",
+      cb,
+    };
+    routes.push(routeObj);
+  };
+
+  post = (route, cb) => {
+    const routeObj = {
+      route,
+      method: "POST",
+      cb,
+    };
+    routes.push(routeObj);
+  };
+
+  put = (route, cb) => {
+    const routeObj = {
+      path: route,
+      method: "PUT",
+      callback: cb,
+    };
+    routes.push(routeObj);
+  };
+
+  delete = (route, cb) => {
+    const routeObj = {
+      path: route,
+      method: "DELETE",
+      callback: cb,
+    };
+    routes.push(routeObj);
   };
 }
+
+export default Server;
