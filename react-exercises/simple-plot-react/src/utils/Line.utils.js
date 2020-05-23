03431b69c6a15734127d7bef6db183b10efa41ee
ajaykumar-mittakola <ajaykumar.mittakola@bankopen.co>
2020-05-23 19:20:32 +0530

    Add line utils test cases

--- react-exercises/simple-plot-react/src/utils/Line.utils.js
+++ react-exercises/simple-plot-react/src/utils/Line.utils.js
@@ -1,4 +1,7 @@
 const getMaxYAxisValue = (data) => {
+    if (!Array.isArray(data)) {
+        throw new TypeError(`Expected an array, got ${typeof data}`);
+    }
     let max = 0;
 
     for (let item of data) {
@@ -12,6 +15,14 @@ const getMaxYAxisValue = (data) => {
 };
 
 const drawAxis = (ctx, xAxisLineSize, yAxisLineSize) => {
+    if (Object.prototype.toString.call(ctx) !== '[object CanvasRenderingContext2D]') {
+        throw new TypeError(`Expected a canvas element, got ${typeof ctx}`);
+    } else if (typeof xAxisLineSize !== 'number') {
+        throw new TypeError(`Expected a number, got ${typeof xAxisLineSize}`)
+    } else if (typeof yAxisLineSize !== 'number') {
+        throw new TypeError(`Expected a number, got ${typeof yAxisLineSize}`)
+    }
+
     ctx.beginPath();
     ctx.strokeStyle = "black";
     ctx.moveTo(xAxisLineSize, 0);
@@ -21,6 +32,9 @@ const drawAxis = (ctx, xAxisLineSize, yAxisLineSize) => {
 };
 
 const drawGrids = (ctx) => {
+    if (Object.prototype.toString.call(ctx) !== '[object CanvasRenderingContext2D]') {
+        throw new TypeError(`Expected a canvas element, got ${typeof ctx}`);
+    }
     let xGrid = 10;
     let yGrid = 10;
     ctx.beginPath();
