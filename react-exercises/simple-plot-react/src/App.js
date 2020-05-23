c97b3b18099fcb9c38c13a0ee5c86d17f10d01d7
ajaykumar-mittakola <ajaykumar.mittakola@bankopen.co>
2020-05-23 19:19:06 +0530

    Move test file to Tests and refactor App component

--- react-exercises/simple-plot-react/src/App.js
+++ react-exercises/simple-plot-react/src/App.js
@@ -4,28 +4,18 @@ import Bar from "./components/Bar/Bar";
 import Line from "./components/Line/Line";
 
 function App() {
-  // const data = [
-  //   { X: 1, Y: 12 },
-  //   { X: 2, Y: 28 },
-  //   { X: 3, Y: 18 },
-  //   { X: 4, Y: 34 },
-  //   { X: 5, Y: 40 },
-  //   { X: 6, Y: 80 },
-  //   { X: 7, Y: 80 },
-  // ];
-
   const data = [
-    { X: 'Aus', Y: 12 },
-    { X: 'Ind', Y: 28 },
-    { X: 'Pak', Y: 18 },
+    { X: 'India', Y: 130 },
+    { X: 'Australia', Y: 28 },
+    { X: 'Japan', Y: 18 },
     { X: 'China', Y: 34 },
-    { X: 'Jap', Y: 40 },
-    { X: 'USA', Y: 80 },
+    { X: 'Korea', Y: 40 },
+    { X: 'USA', Y: 35 },
     { X: 'UK', Y: 80 },
   ];
   return (
     <div className="App">
-      <h1>Working</h1>
+      <h1>Simple Plot React</h1>
       <Line data={data} lineGap={100} />
       <Bar />
     </div>
